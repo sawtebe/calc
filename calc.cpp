25adcd8d3b1004ac123d69dca28e1ee14baa9bc5
sevag <sevag@DESKTOP-NUO94C3>
2024-04-29 14:13:21 +0700

    Добавьте файлы проекта.

---
+++ calc.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using std::cout;
+
+int main() {
+    double a, b;
+    char op;
+    std::cin >> a >> op >> b;
+
+    switch (op) {
+    case '+':
+        cout << a + b;
+        break;
+    case '-':
+        cout << a - b;
+        break;
+    case '*':
+        cout << a * b;
+        break;
+    case '/':
+        if (b == 0) {
+            cout << "Division by zero";
+            return -1;
+        }
+        cout << a / b;
+        break;
+    default:
+        cout << "Wrong operation";
+        return -1;
+    }
+}
