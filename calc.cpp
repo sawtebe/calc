44ab20bf2ad77e22266402f09b58f432fc09c91c
sawtebe <seva.garin@gmail.com>
2024-04-29 14:29:32 +0700

    .

--- calc.cpp
+++ calc.cpp
@@ -2,29 +2,5 @@
 using std::cout;
 
 int main() {
-    double a, b;
-    char op;
-    std::cin >> a >> op >> b;
-
-    switch (op) {
-    case '+':
-        cout << a + b;
-        break;
-    case '-':
-        cout << a - b;
-        break;
-    case '*':
-        cout << a * b;
-        break;
-    case '/':
-        if (b == 0) {
-            cout << "Division by zero";
-            return -1;
-        }
-        cout << a / b;
-        break;
-    default:
-        cout << "Wrong operation";
-        return -1;
-    }
+	cout << "Hello world!" << std::endl;
 }
